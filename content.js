93f6ada9161f7a866349e0160491597af949db18
kevinViers <112672086+kevinViers@users.noreply.github.com>
2023-08-14 16:59:11 -0700

    added all files via upload
    zip file is just the rest of the files pre-zipped.

---
+++ content.js
@@ -0,0 +1,22 @@
+let replacementCount = 0;
+
+function replaceText(node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+        const matches = node.textContent.match(/Winston/g);
+        if (matches) {
+            replacementCount += matches.length;
+            node.textContent = node.textContent.replace(/Winston/g, 'Winton');
+        }
+    } else {
+        for (let i = 0; i < node.childNodes.length; i++) {
+            replaceText(node.childNodes[i]);
+        }
+    }
+}
+
+browser.storage.local.get('isEnabled', function(data) {
+    if (data.isEnabled) {
+        replaceText(document.body);
+        browser.storage.local.set({replacementCount: replacementCount});
+    }
+});
