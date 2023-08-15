161d99b067333ec202af0a01ae77c73065f89367
kevinViers <112672086+kevinViers@users.noreply.github.com>
2023-08-14 17:05:16 -0700

    enabled winton replacer to be on by default

--- popup.js
+++ popup.js
@@ -3,20 +3,17 @@ let isEnabled = false;
 document.getElementById('toggle').addEventListener('change', function(event) {
     isEnabled = event.target.checked;
     if (isEnabled) {
-        // Send message to content script to replace text
         browser.tabs.executeScript({
             code: 'replaceText(document.body);'
         });
     }
-    // You can add more functionality here to revert changes if you wish
 });
 
-// Retrieve replacement count from storage and update the popup
 browser.storage.local.get('replacementCount', function(data) {
     document.getElementById('count').textContent = data.replacementCount || 0;
 });
 
-// Update the checkbox based on the current state
 browser.storage.local.get('isEnabled', function(data) {
-    document.getElementById('toggle').checked = data.isEnabled || false;
+    document.getElementById('toggle').checked = data.isEnabled !== undefined ? data.isEnabled : true;
 });
+
